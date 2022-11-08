1d346c14b77272419846aae106e6d8a3ec7ffd75
Nick Collier <ncollier@anl.gov>
2022-11-08 16:27:37 -0500

    Added command line input parameters via json string

--- exp/chicago_yr1/chicago_yr1.cpp
+++ exp/chicago_yr1/chicago_yr1.cpp
@@ -1,5 +1,8 @@
+#include <ostream>
+
 #include "Time_Series.h"
 #include "Event_Driven_NUCOVID.h"
+#include "json.hpp"
 
 vector<vector<double>> transpose2dVector( vector<vector<double>> vec2d ) {
     vector<vector<double>> tvec2d;
@@ -20,12 +23,13 @@ vector<vector<double>> transpose2dVector( vector<vector<double>> vec2d ) {
     return tvec2d;
 }
 
-vector<Node*> initialize_1node() {
+vector<Node*> initialize_1node(const nlohmann::json& params) {
     vector<Node*> nodes;
     int N = 2500000;
     vector<double> Ki; // S -> E transition rate
 
-    double ini_Ki = 1.0522;
+    // double ini_Ki = 1.0522;
+    double ini_Ki = params["ini_Ki"];
     vector<TimeSeriesAnchorPoint> Ki_ap = {
         {0, 1.0     },
         {28, 0.6263 },
@@ -47,9 +51,11 @@ vector<Node*> initialize_1node() {
     Ki = stepwiseTimeSeries(Ki_ap);
     //Ki = linInterpolateTimeSeries(Ki_ap);
 
-    double Kasymp= 0.4066/3.677037; // E -> I (asymp) rate
+    // double Kasymp= 0.4066/3.677037; // E -> I (asymp) rate
+    double Kasymp = params["nmrtr_Kasymp"].get<double>()/3.677037;
     double Kpres = 0.5934/3.677037; // E -> I (symp) rate
-    double Kmild = 0.921/3.409656;
+    // double Kmild = 0.921/3.409656;
+    double Kmild = params["nmrtr_Kmild"].get<double>()/3.409656;
     double Kseve = 0.079/3.409656;
     double Khosp = 1.0/4.076704;
     double Kcrit = 1.0/5.592791;
@@ -148,9 +154,10 @@ vector<Node*> initialize_1node() {
     vector<vector<double>> Pdetect{Pdet_asym, Pdet_pres, Pdet_symm, Pdet_syms};
     Pdetect = transpose2dVector(Pdetect);
 
-    double frac_infectiousness_As = 0.8;
-    double frac_infectiousness_det = 0.00733;
-    //double frac_infectiousness_det = 0.2;
+    // double frac_infectiousness_As = 0.8;
+    double frac_infectiousness_As = params["frac_infectiousness_As"];
+    // double frac_infectiousness_det = 0.00733;
+    double frac_infectiousness_det = params["frac_infectiousness_det"];
     vector<double> time_to_detect = {1.904861, 7, 2};
 
     Node* n1 = new  Node(0, N, Ki, Kasymp, Kpres, Kmild, Kseve, Khosp, Kcrit,
@@ -160,11 +167,14 @@ vector<Node*> initialize_1node() {
     return(nodes);
 }
 
-void runsim (int serial) {
-    cout << "Running Sim " << to_string(serial) << endl;
+void runsim (const nlohmann::json& params) {
+    cout << "Running Sim"<< endl;
+    for (auto& el : params.items()) {
+        std::cout << el.key() << " : " << el.value() << std::endl;
+    }
 
     vector<string> out_buffer;
-    vector<Node*> nodes = initialize_1node();
+    vector<Node*> nodes = initialize_1node(params);
     vector<vector<double>> infection_matrix;
     for(int i = 0; i < 1; i++) {
         vector<double> inf_prob;
@@ -173,21 +183,60 @@ void runsim (int serial) {
     }
 
     Event_Driven_NUCOVID sim(nodes, infection_matrix);
-    sim.rng.seed(serial);
+    sim.rng.seed(params["random_seed"]);
     sim.Now = 9;
     sim.rand_infect(10, nodes[0]);//*2
     out_buffer = sim.run_simulation(371, false);
 
-    string out_fname = "/projects/b1139/covid-age-output/chicago_1yr/daily_output." + to_string(serial);
+    string out_fname = params["output_directory"].get<string>()+ "/" +
+        params["output_filename"].get<string>();
     write_buffer(out_buffer, out_fname, true);
 
     return;
 }
 
+void usage() {
+    std::cerr << "usage: model [json map formatted parameters]" << std::endl;
+}
+
+int parse_params(nlohmann::json& params, const std::string& cl_params) {
+    nlohmann::json j2 = nlohmann::json::parse(cl_params);
+    for (auto& el : j2.items()) {
+        string key = el.key();
+        if (!params.contains(key)) {
+            std::cerr << "Invalid parameter: " + key << std::endl;
+            return -1;
+        }
+        params[key] = el.value();
+        // std::cout << el.key() << " : " << el.value() << "\n";
+    }
+    return 0;
+}
+
+void load_default_params(nlohmann::json& params) {
+    // DEFAULT PARAMETERS
+    params["output_directory"] = "./";
+    params["output_filename"] = "daily_output.txt";
+    params["random_seed"] = 42;
+    params["nmrtr_Kasymp"] = 0.4066;
+    params["nmrtr_Kmild"] = 0.921;
+    params["ini_Ki"] = 1.0522;
+    params["frac_infectiousness_As"] = 0.8;
+    params["frac_infectiousness_det"] =  0.00733;
+}
+
 int main(int argc, char* argv[]) { 
-    int serial = std::stoi(argv[1]);
-
-    runsim(serial);
-
+    if (argc > 2) {
+        usage();
+        return -1;
+    } else {
+        nlohmann::json params;
+        load_default_params(params);
+        if (argc == 2) {
+            int ret_val = parse_params(params, argv[1]);
+            if (ret_val != 0) return -1;
+        }
+        runsim(params);
+    }
     return 0;
 }
